f671df843a06f7775547b775f1f71600a315cb0e
nkkf <nelsonkenmochi@gmail.com>
2020-06-02 22:30:07 -0400

    feat: dia 2 - backend quase finalizado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
+import path from 'path';
+import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json([
-        'Diego',
-        'Nelson',
-        'Jso'
-    ])
-});
+app.use(cors());
+
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));    
 
 app.listen(3000);
